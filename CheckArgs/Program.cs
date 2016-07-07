28312b035f5f0c4db83396e041a83fb82de2de3b
fernandoespinosa <fernandoespinosa@fernandoespinosa.org>
2016-07-07 14:05:27 -0400

    Initial commit.

---
+++ CheckArgs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NinjackeR.Pompidou;
+
+namespace CheckArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Environment.CommandLine");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine(Environment.CommandLine);
+            Console.WriteLine();
+            Console.WriteLine("Environment.GetCommandLineArgs()");
+            Console.WriteLine("--------------------------------");
+            Environment.GetCommandLineArgs().ForEach((s, i) => Console.WriteLine("{0}: {1}", i, s));
+            Console.WriteLine();
+            Console.WriteLine("static void Main(string[] args)");
+            Console.WriteLine("-------------------------------");
+            args.ForEach((s, i) => Console.WriteLine("{0}: {1}", i, s));
+        }
+    }
+}
